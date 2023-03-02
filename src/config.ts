61b4970371bd3d58788dcebd62b8cb78ee238948
Viet <viet@r-digital.tech>
2023-03-02 14:15:18 +0700

    Change environment variable

--- src/config.ts
+++ src/config.ts
@@ -5,31 +5,30 @@ import {
   ephemeralConnectorType,
   WalletConnectConnector,
 } from '@concordium/react-components';
+import { env } from './env';
 
-const TESTNET_GENESIS_BLOCK_HASH =
-  '4221332d34e1694168c2a0c0b3fd0f273809612cb13d000d5c2e00e85f50f796';
-const MAINNET_GENESIS_BLOCK_HASH =
-  '9dd9ca4d19e9393877d2c44b70f89acbfc0883c2243e5eeaecc0d1cd0503f478';
+const TESTNET_GENESIS_BLOCK_HASH = env.REACT_APP_TESTNET_GENESIS_BLOCK_HASH;
+const MAINNET_GENESIS_BLOCK_HASH = env.REACT_APP_MAINNET_GENESIS_BLOCK_HASH;
 
 export const TESTNET: Network = {
   name: 'testnet',
   genesisHash: TESTNET_GENESIS_BLOCK_HASH,
-  jsonRpcUrl: 'https://json-rpc.testnet.concordium.com',
-  ccdScanBaseUrl: 'https://testnet.ccdscan.io',
+  jsonRpcUrl: env.REACT_APP_TESTNET_JSON_RPC_URL,
+  ccdScanBaseUrl: env.REACT_APP_TESTNET_CCD_SCAN_BASE_URL,
 };
 export const MAINNET: Network = {
   name: 'mainnet',
   genesisHash: MAINNET_GENESIS_BLOCK_HASH,
-  jsonRpcUrl: 'https://json-rpc.mainnet.concordium.software',
-  ccdScanBaseUrl: 'https://ccdscan.io',
+  jsonRpcUrl: env.REACT_APP_MAINNET_JSON_RPC_URL,
+  ccdScanBaseUrl: env.REACT_APP_MAINNET_CCD_SCAN_BASE_URL,
 };
 
-const WALLET_CONNECT_PROJECT_ID = '76324905a70fe5c388bab46d3e0564dc';
+const WALLET_CONNECT_PROJECT_ID = env.REACT_APP_WALLET_CONNECT_PROJECT_ID;
 const WALLET_CONNECT_OPTS: SignClientTypes.Options = {
   projectId: WALLET_CONNECT_PROJECT_ID,
   metadata: {
-    name: 'Contract Update',
-    description: 'Example dApp for the performing an update on a contract.',
+    name: 'AesirX Contract Update',
+    description: 'AesirX dApp update on a contract.',
     url: '#',
     icons: ['https://walletconnect.com/walletconnect-logo.png'],
   },
